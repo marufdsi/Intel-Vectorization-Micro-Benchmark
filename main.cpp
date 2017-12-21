8a69deb33618a6e53d76c6feab6721519f95f986
Maruf <maruf.csea1@gmail.com>
2017-12-21 15:58:23 -0500

    Make Pull Request

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int checkVectorization(int startNode, int frameSize) {
     int node;
     for (int start = 0; start < frameSize; ++start) {
         int nextJump = start * frameSize;
-        node = stratNode + nextJump;
+        node = startNode + nextJump;
     }
     return block_size;
 }
@@ -34,7 +34,6 @@ int main() {
     std::cout << "Hello World" << std::endl;
     int value = 0;
     int a[100], b[100], c[100];
-    int u = getLoopConditon();
     int node;
     checkVectorization(0, 16);
     std::cout << "Value of c[50] = " << c[50] << std::endl;
