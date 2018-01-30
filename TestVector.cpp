6ef220a6624f7b67e5d9fb94bccac13572cbbbda
Maruf <maruf.csea1@gmail.com>
2018-01-29 21:05:13 -0500

    Updated

--- TestVector.cpp
+++ TestVector.cpp
@@ -17,13 +17,13 @@ int main(){
     typedef double edgeweight;
     count z=100;
     count zeta[z];
-    index min_deg[4];
+    index _deg[16];
     count block_size = 16;
     index neighbor_count[block_size] __attribute__((aligned(32)));
     node nodes[block_size] __attribute__((aligned(32)));
-    const node* temp_outEdges[block_size] __attribute__((aligned(32)));
-    std::vector<edgeweight> *affinity_pointer __attribute__((aligned(32)));
-    index* neighbor_community[block_size] __attribute__((aligned(32)));
+    const node temp_outEdges[block_size][z] __attribute__((aligned(32)));
+    edgeweight affinity_pointer[block_size][z] __attribute__((aligned(32)));
+    index neighbor_community[block_size][z] __attribute__((aligned(32)));
     const edgeweight * temp_outEdgeWeight[block_size] __attribute__((aligned(32)));
     node u=4;
     std::vector<edgeweight> affinity(z);
@@ -33,21 +33,15 @@ int main(){
 
     #pragma omp simd collapse(2) safelen(4)
     for (node counter = 0; counter < block_size; ++counter) {
-        node _node = nodes[counter];
         #pragma omp simd
-        for (index _edge = 0; _edge < min_deg[counter]; ++_edge) {
+        for (index _edge = 0; _edge < _deg[counter]; ++_edge) {
             node v = temp_outEdges[counter][_edge];
             index C = zeta[v];
-            edgeweight* affinity_u = &affinity_pointer[counter][C];
-            if (_node != v) {
-                if (*affinity_u == -1) {
-                    // found the neighbor for the first time, initialize to 0 and add to list of neighboring communities
-                    *affinity_u = 0;
-                    neighbor_community[counter][neighbor_count[counter]] = C;
-                    neighbor_count[counter] += 1;
-                }
-                *affinity_u += temp_outEdgeWeight[counter][_edge];
-            }
+            // found the neighbor for the first time, initialize to 0 and add to list of neighboring communities
+            affinity_pointer[counter][C] = 0;
+            neighbor_community[counter][neighbor_count[counter]] = C;
+            neighbor_count[counter] += 1;
+            affinity_pointer[counter][C] += temp_outEdgeWeight[counter][_edge];
         }
     }
 
