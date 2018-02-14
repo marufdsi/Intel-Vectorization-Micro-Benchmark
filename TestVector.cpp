6903230396f7983e0ad42994b89a377e31378c92
Maruf <maruf.csea1@gmail.com>
2018-02-13 20:54:09 -0500

    Updated

--- TestVector.cpp
+++ TestVector.cpp
@@ -16,7 +16,7 @@ int main(){
     typedef int32_t node;
     typedef int32_t count;
     typedef double edgeweight;
-    indec test_value = -1;
+    index test_value = -1;
     cout<<std::signbit(test_value)<<endl;
     /*count z=100;
     count zeta[z];
