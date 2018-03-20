0b06e3b696179e77d05a4484ed833dca4519b508
Md Maruf Hossain <mhossa10@cph-i2.uncc.edu>
2018-03-20 15:44:13 -0400

    Add TestVector.cpp

--- TestVector.cpp
+++ TestVector.cpp
@@ -1,38 +1,26 @@
 #include<iostream>
-#include<vector>
-#include <omp.h>
-#include <cmath>
-#include <chrono>
+#include<chrono>
 using namespace std;
-
-
-
-/*#pragma omp declare simd uniform(a), linear(i:1), simdlen(4)
-#pragma omp declare simd uniform(a), simdlen(4)
-void foo(int *a, int i){
-  std::cout<<a[i]<<"\n";
-}*/
-
 double checkVector(int n){
     double A[n], B[n];
     for (int i = 0; i < n; ++i) {
         A[i] = 1.0;
         B[i] = 1.0;
     }
-
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
     for (int i = 0; i < n; ++i) {
-        A[i] += B[i];
+//      	if(i%4 == 0){
+            A[i] += B[i];
+//	}
     }
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout<<"Time Elapsed: "<<elapsed_time.count()<<std::endl;
     return A[0];
 }
+
 int main(){
-  std::chrono::time_point<std::chrono::system_clock> start, end;
-  start = std::chrono::system_clock::now();
-  checkVector(5000000);
-  end = std::chrono::system_clock::now();
-  std::chrono::duration<double> elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(
-                        end - start);
-  double elapsedTime = elapsed_seconds.count();
-  cout<<"Time Required: "<<elapsedTime<<endl;
-  return 0;
+	checkVector(999999999);
+	return 0;
 }
