04d2bb1e30d43409ac06e56e9f84ee8ff78f5d3d
maruf <maruf.csea1@gmail.com>
2018-07-18 13:30:21 -0400

    Make Pull Request

--- TestIntrinsic.cpp
+++ TestIntrinsic.cpp
@@ -20,7 +20,7 @@ int main(){
 
 
   /******/
-  index _deg = 100, u=0;
+  index _deg = 10000, u=0;
   count neigh_counter = 0;
   count vertex_count = 0;
   node *pnt_outEdges, *outEdges, *zeta;
@@ -35,7 +35,7 @@ int main(){
 #pragma omp simd
   for(index edge=0; edge<_deg; ++edge){
     outEdges[edge] = edge;
-    zeta[edge] = edge%5;
+    zeta[edge] = edge%20;
   }
   pnt_outEdges = &outEdges[0];
   edgeweight defaultEdgeWeight = 1.0;
