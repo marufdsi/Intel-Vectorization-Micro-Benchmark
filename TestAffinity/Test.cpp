302ac915fd4a47d7a3dcc6a7d77e81b7f0ea8d37
Md Maruf Hossain <maruf.csea1@gmail.com>
2020-01-27 20:56:59 -0500

    benchmark updated

--- TestAffinity/Test.cpp
+++ TestAffinity/Test.cpp
@@ -15,11 +15,17 @@
 #include <sstream>
 #include "Util.h"
 
-
-
-
 int main (){
     std::string fileName;
     int ppn;
+    float * ptr = (float *) malloc(10 * sizeof(float));
+    for(int i=0; i<10; ++i){
+      std::cout << ptr[i] << " "; 
+    }
+    std::cout<<std::endl;
+    for(int i=0; i<5000; ++i){
+      std::cout << ptr[i] << " ";
+    }
+    std::cout<<std::endl;
     return 0;
 }
--- TestVectorOperation/bench_scatter.sh
+++ TestVectorOperation/bench_scatter.sh
@@ -2,12 +2,15 @@
 
 rm init*.csv
 
+./TestIntrinsic 36
+./TestIntrinsic 32
+./TestIntrinsic 18
 ./TestIntrinsic 1
 ./TestIntrinsic 2
 ./TestIntrinsic 4
 ./TestIntrinsic 16
-./TestIntrinsic 32
-./TestIntrinsic 64
-./TestIntrinsic 128
-./TestIntrinsic 256
+
+#./TestIntrinsic 64
+#./TestIntrinsic 128
+#./TestIntrinsic 256
 
